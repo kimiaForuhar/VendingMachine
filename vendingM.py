f5940ca6d050036836b5466ab9a5263d618690fc
kimya <kimyafruhr@aut.ac.ir>
2020-07-23 19:44:17 +0430

    class product added

--- vendingM.py
+++ vendingM.py
@@ -1,20 +1,28 @@
 class Vending:
-    def __init__(self,id):
-        self.id=id
-        self.saleamount=0
-        self.salecount=0
-        self.products={}
+    def __init__(self, id):
+        self.id = id
+        self.saleamount = 0
+        self.salecount = 0
+        self.products = {}
 
-    def buy(self,product):
+    def buy(self, product):
         if product in self.products.items():
-            if self.products[product]>0:
-                self.products[product]-=1
-                self.salecount+=1
-                self.saleamount+=int(product.price)
+            if self.products[product] > 0:
+                self.products[product] -= 1
+                self.salecount += 1
+                self.saleamount += int(product.price)
                 return True
             return False
 
-    def addprod(self,product,amount):
+    def addprod(self, product, amount):
         if product in self.products:
-            self.products[product]+=int(amount)
-        else: self.products[product]=int(amount)
+            self.products[product] += int(amount)
+        else:
+            self.products[product] = int(amount)
+        
+
+class Product:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
