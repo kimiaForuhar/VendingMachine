ed3f581cb94b05b0f154d39d6117b46f8414da51
kimya <kimyafruhr@aut.ac.ir>
2020-07-24 19:21:33 +0430

    bugs fixed. project is done

--- vendingM.py
+++ vendingM.py
@@ -1,19 +1,20 @@
 class Vending_machine:
+    # products={}
     def __init__(self, id):
         self.id = id
         self.saleamount = 0
         self.salecount = 0
         self.products = {}
 
-
     def buy(self, product):
         if product in self.products.keys():
-            if self.products[product] > 0:
+            if self.products[product] >= 1:
                 self.products[product] -= 1
                 self.salecount += 1
-                self.saleamount += int(Product(id=product,).getprice(product))
-            #     return True
-            # return False
+                for i in productList:
+                    if i.id == product:
+                        self.saleamount += int(i.price)
+                        break
 
     def addprod(self, product, amount):
         if product in self.products.keys():
@@ -23,17 +24,10 @@ class Vending_machine:
 
 
 class Product:
-    # prod = {}
-
     def __init__(self, id, name, price):
         self.id = id
         self.name = name
         self.price = price
-
-    def getprice(self, id):
-        for i in productList:
-            if i.id == id:
-                return i.price
 
 
 m = int(input())
@@ -44,8 +38,6 @@ for i in range(m):
     name = input()
     price = int(input())
     productList.append(Product(id, name, price))
-    # Product.prod[id]=[name,price]
-# print(productList[1].price)
 
 for j in range(int(input())):
     vendingList.append(Vending_machine(j))
@@ -55,7 +47,7 @@ for j in range(int(input())):
 
 for t in range(int(input())):
     inp = input().split()
-    vendingList[int(inp[0]) - 1].buy(int(inp[1]) - 1)
+    vendingList[int(inp[0]) - 1].buy(int(inp[1]))
 
 for h in range(len(vendingList)):
-    print(h+1, ":", vendingList[h].saleamount, ",", vendingList[h].salecount)
+    print(h + 1, ":", vendingList[h].salecount, ",", vendingList[h].saleamount)
